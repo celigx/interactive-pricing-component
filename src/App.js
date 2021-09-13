1fcbcf80ac974713da7aa31894f56543e423d04d
Celig <cela@protonmail.com>
2021-09-13 18:02:20 +0200

    Add title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.sass';
 function App() {
   return (
     <div className="app">
-
+      <div className="titleContainer">
+        <h1 className="title">Simple, traffic-based pricing</h1>
+        <p className="subTitle">Sign-up for our 30-day trial. No credit card required.</p>
+      </div>
     </div>
   );
 }
