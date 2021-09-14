2fadbe4e82e9b250a591f52224571897245b8123
Celig <cela@protonmail.com>
2021-09-14 14:29:56 +0200

    Add data object

---
+++ src/Utils/DataUtils.js
@@ -0,0 +1,22 @@
+export const data = {
+  1: {
+    views: '10K',
+    price: 8
+  },
+  2: {
+    views: '50K',
+    price: 12
+  },
+  3: {
+    views: '100K',
+    price: 16
+  },
+  4: {
+    views: '500K',
+    price: 24
+  },
+  5: {
+    views: '1M',
+    price: 36
+  }
+}
