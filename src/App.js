49778871ec614f4d7ca6c27bb2438b87fae4d34d
Celig <cela@protonmail.com>
2021-09-14 14:31:47 +0200

    Add update pageviews and price on slider change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import './App.sass';
+import { data } from './Utils/DataUtils';
 
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
@@ -9,6 +11,17 @@ import checkmark from './assets/icon-check.svg';
 import sliderArrow from './assets/icon-slider.svg';
 
 function App() {
+  const [pageViews, setPageViews] = useState(data[3].views)
+  const [slider, setSlider] = useState(3)
+  const [toggle, setToggle] = useState(false)
+  const [price, setPrice] = useState(data[3].price)
+
+  const handleSlider = (e) => {
+    setSlider(e)
+    setPageViews(data[e].views)
+    setPrice(data[e].price)
+  }
+
   return (
     <div className="app">
 
@@ -20,7 +33,7 @@ function App() {
       <div className="cardContainer">
 
         <div className="topContainer">
-          <h1 className="pageViews">100K PAGEVIEWS</h1>
+          <h1 className="pageViews">{pageViews} PAGEVIEWS</h1>
           <Slider
             min={1}
             max={5}
@@ -28,9 +41,10 @@ function App() {
             trackStyle={[{ backgroundColor: '#a5f3eb', height: '8px' }]}
             handleStyle={[{ backgroundColor: '#10d5c2', height: '40px', width: '40px', marginTop: '-15px', border: 'none', backgroundImage: `url(${sliderArrow})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', cursor: 'pointer', boxShadow: '0px 15px 25px 8px #a5f3eb' }]}
             railStyle={{ height: '8px' }}
+            onChange={handleSlider}
           />
           <div className="priceContainer">
-            <h1 className="price">$16.00</h1>
+            <h1 className="price">{price}</h1>
             <p className="month">/ month</p>
           </div>
           <div className="toggleContainer">
