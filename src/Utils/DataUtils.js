66deb596a1c01eaa3c2e9ade983f41eb9fd752a0
Celig <cela@protonmail.com>
2021-09-14 16:11:55 +0200

    Change objects property

--- src/Utils/DataUtils.js
+++ src/Utils/DataUtils.js
@@ -1,22 +1,22 @@
 export const data = {
   1: {
-    views: '10K',
+    pageViews: '10K',
     price: 8
   },
   2: {
-    views: '50K',
+    pageViews: '50K',
     price: 12
   },
   3: {
-    views: '100K',
+    pageViews: '100K',
     price: 16
   },
   4: {
-    views: '500K',
+    pageViews: '500K',
     price: 24
   },
   5: {
-    views: '1M',
+    pageViews: '1M',
     price: 36
   }
 }
